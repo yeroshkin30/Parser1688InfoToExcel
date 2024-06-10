8b3957f145e88c403520528ff531030ba4fba655
eroha <oyeroshkin30@gmail.com>
2024-06-10 03:09:37 +0300

    Initial Commit

---
+++ testJSON/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  testJSON
+//
+//  Created by oleh yeroshkin on 10.06.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
